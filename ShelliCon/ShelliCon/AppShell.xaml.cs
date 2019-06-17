eb4382b53d3c97b95026f3f3b24852968908d219
Chris Miller <chris@rajapet.net>
2019-06-17 14:11:59 -0400

    Added actual source code

---
+++ ShelliCon/ShelliCon/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShelliCon
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
